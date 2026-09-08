8418d2ea27368fb3df5903257488947b848e594f
zainab-16-marker <zaynp01602@gmail.com>
2026-09-08 09:26:50 +0300

    startup

---
+++ server.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const logger = require('morgan');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// Routes go here
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
